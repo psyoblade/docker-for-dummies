6c7812adf18d25e2d79eafa2f85731dc9e5a3eb3
psyoblade <park.suhyuk@gmail.com>
2020-04-28 23:43:40 +0900

    [이동] psyoblade/docker-sqoop 으로 이동

--- sqoop/sqoop.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "docker run --rm --name sqoop --network=sqoop-mysql -v `pwd`/jars:/usr/local/sqoop/jars -dit dvoros/sqoop-hive:2.3.3"
-echo "cp jars/*.jar lib"
-echo "mkdir target"
-echo "bin/sqoop import -fs local -jt local -m 1 --driver com.mysql.jdbc.Driver --connect jdbc:mysql://mysql:3306/psyoblade --table users --target-dir /usr/local/sqoop/target/users --verbose --username root --relaxed-isolation"
